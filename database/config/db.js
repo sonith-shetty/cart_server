2070df4ba57f63de587b575f6341c5787b4ba3aa
sonith-shetty <123190511+sonith-shetty@users.noreply.github.com>
2024-09-25 02:14:23 +0000

    Update db.js

--- database/config/db.js
+++ database/config/db.js
@@ -2,6 +2,6 @@ const mysql = require('mysql2');
 const { host, user, database, password, db_url } = require('../../utils/secret.utils');
 
 const db_port = 25686;
-const pool = mysql.createPool(db_url || { host, user, database, password, port }).promise();
+const pool = mysql.createPool(db_url || { host, user, database, password, db_port }).promise();
 
 module.exports = pool;
