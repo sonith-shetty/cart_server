acb974e3314ee5abc8edc4095ae925c0449e622d
sonith-shetty <123190511+sonith-shetty@users.noreply.github.com>
2024-09-29 15:12:42 +0530

    Update auth.util.js

--- utils/auth.util.js
+++ utils/auth.util.js
@@ -4,7 +4,7 @@ const { jwt_key } = require('./secret.utils');
 const auth_token_user = (req, res, next) => {
     const { user_access_token } = req.cookies;
     if(!user_access_token) {
-        return res.status(401).json({ status: "error", data: "invalid user", error: err });
+        throw new Error("Invalid user");
     }
     try {
         const isUser = jwt.verify(user_access_token, jwt_key);
