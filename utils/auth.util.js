09a0a6999eaaa71dabab49c62fc4ed2727efc958
sonith-shetty <123190511+sonith-shetty@users.noreply.github.com>
2024-09-29 15:49:33 +0530

    Update auth.util.js

--- utils/auth.util.js
+++ utils/auth.util.js
@@ -2,46 +2,52 @@ const jwt = require('jsonwebtoken');
 const { jwt_key } = require('./secret.utils');
 
 const auth_token_user = (req, res, next) => {
-    console.log(req.cookies);
-    const { user_access_token } = req.cookies;
-    if(!user_access_token) {
-        throw new Error("Invalid user");
+    const user_access_token = req.cookies?.user_access_token;
+
+    if (!user_access_token) {
+        return res.status(401).json({ status: "error", data: "Invalid user" });
     }
+
     try {
-        const isUser = jwt.verify(user_access_token, jwt_key);
-        const { contact, username } = isUser;
+        const decodedToken = jwt.verify(user_access_token, jwt_key);
+        const { contact, username } = decodedToken;
+
         req.user = {
             contact,
             username,
-        }
+        };
+
         next();
     } catch (err) {
-        return res.status(401).json({ status: "error", data: "invalid user", error: err });
+        return res.status(401).json({ status: "error", data: "Invalid user", error: err.message });
     }
 }
 
 const auth_token_admin = (req, res, next) => {
-    const { user_access_token } = req.cookies;
-    if(!user_access_token) {
-        return res.status(401).json({ status: "error", data: "invalid user", error: err });
+    const user_access_token = req.cookies?.user_access_token;
+
+    if (!user_access_token) {
+        return res.status(401).json({ status: "error", data: "Invalid user" });
     }
+
     try {
-        const isUser = jwt.verify(user_access_token, jwt_key);
-        const { contact, username, isAdmin } = isUser;
+        const decodedToken = jwt.verify(user_access_token, jwt_key);
+        const { contact, username, isAdmin } = decodedToken;
+
         req.user = {
             contact,
             username,
             isAdmin
+        };
+
+        if (!isAdmin) {
+            return res.status(403).json({ status: "error", data: "Access denied" });
         }
-        if(!isAdmin) {
-            return res.status(401).json({ status: "error", data: "invalid user", error: err });
-        }
+
         next();
     } catch (err) {
-        return res.status(401).json({ status: "error", data: "invalid user", error: err });
+        return res.status(401).json({ status: "error", data: "Invalid user", error: err.message });
     }
 }
-
-
 
 module.exports = { auth_token_admin, auth_token_user };
