0f1aa709e2b5574e0b3c61187a82512c4cfc3830
sonith-shetty <123190511+sonith-shetty@users.noreply.github.com>
2024-10-11 11:23:05 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,46 +1,45 @@
 const express = require('express');
-const http = require('http');
+const app = express();
+require('dotenv').config();
+const httpServer = require('http').createServer(app);
 const { Server } = require('socket.io');
+
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-require('dotenv').config();
 
 const { port, client_url } = require('./utils/secret.utils');
-const { socket } = require('./controllers/bill.controller');
+const { auth_token_admin } = require('./utils/auth.util');
 
 const userRouter = require('./routers/user.router');
 const adminRouter = require('./routers/admin.router');
 const billRouter = require('./routers/bill.router');
 
-const app = express();
-const httpServer = http.createServer(app);
-const io = new Server(httpServer, {
-    cors: {
-        origin: client_url || 'http://localhost:3000',
-        methods: ['GET', 'POST'],
-        credentials: true, // Allow credentials to be sent
+var whitelist = [
+    client_url,
+    "http://localhost:3000",
+];
+var corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
     },
-});
+    credentials: true,
+    methods: ["GET", "DELETE", "POST"],
+    allowedHeaders: ["Content-Type"],
+    exposedHeaders: ["set-cookie"],
+};
 
-// Socket.IO connection
-socket(io);
+app.use(cookieParser());
+app.use(express.json());
+app.use(cors(corsOptions))
 
-// Middleware setup
-app.use(cors({
-    origin: client_url || 'http://localhost:3000',
-    credentials: true, // Allow credentials to be sent
-}));
-
-app.use(cookieParser()); // Middleware to parse cookies
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json()); // Middleware to parse JSON bodies
-
-// Route handlers
 app.use(userRouter);
 app.use(billRouter);
 app.use(adminRouter);
 
-// Start the server
 httpServer.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`port ${port}`);
 });
